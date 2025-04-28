5e3fca3e7385cdd63e6d20eaedb70cfb3374cd0b
Beernaert Robbe <robbe.beernaert@student.howest.be>
2025-04-28 14:16:08 +0200

    updated sensor shield example

--- examples/SensorShield/SensorShield.ino
+++ examples/SensorShield/SensorShield.ino
@@ -9,27 +9,27 @@
  *
  * Copyright (C) 2023, DPTechnics bv
  * All rights reserved.
- * 
+ *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions are met:
- * 
+ *
  *   1. Redistributions of source code must retain the above copyright notice,
  *      this list of conditions and the following disclaimer.
- * 
+ *
  *   2. Redistributions in binary form must reproduce the above copyright
  *      notice, this list of conditions and the following disclaimer in the
  *      documentation and/or other materials provided with the distribution.
- * 
+ *
  *   3. Neither the name of DPTechnics bv nor the names of its contributors may
  *      be used to endorse or promote products derived from this software
  *      without specific prior written permission.
- * 
+ *
  *   4. This software, with or without modification, must only be used with a
  *      Walter board from DPTechnics bv.
- * 
+ *
  *   5. Any software provided in binary form under this license must not be
  *      reverse engineered, decompiled, modified and/or disassembled.
- * 
+ *
  * THIS SOFTWARE IS PROVIDED BY DPTECHNICS BV “AS IS” AND ANY EXPRESS OR IMPLIED
  * WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE IMPLIED WARRANTIES OF
  * MERCHANTABILITY, NONINFRINGEMENT, AND FITNESS FOR A PARTICULAR PURPOSE ARE
@@ -42,19 +42,20 @@
  * SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
  *
  * @section DESCRIPTION
- * 
+ *
  * This file contains a sketch which reads out environimental sensors and sends
  * a reading of these sensors to the Walter demo server every 60 seconds.
  */
 
-#include <esp_mac.h>
+#include <Adafruit_MPL3115A2.h>
+#include <HardwareSerial.h>
 #include <LTR329ALS01.h>
 #include <WalterModem.h>
-#include <HardwareSerial.h>
-#include <Adafruit_MPL3115A2.h>
+#include <esp_mac.h>
+
 
 /**
- * @brief The address of the server to upload the data to. 
+ * @brief The address of the server to upload the data to.
  */
 #define SERV_ADDR "64.225.64.140"
 
@@ -84,6 +85,11 @@
 WalterModem modem;
 
 /**
+ * @brief Response object containing command response information.
+ */
+WalterModemRsp rsp;
+
+/**
  * @brief Lux sensor instance.
  */
 LTR329ALS01 ltr329(9, 8);
@@ -97,279 +103,196 @@ Adafruit_MPL3115A2 mpl3115a2 = Adafruit_MPL3115A2();
  * @brief The buffer to transmit to the UDP server. The first 6 bytes will be
  * the MAC address of the Walter this code is running on.
  */
-uint8_t dataBuf[14] = { 0 };
+uint8_t dataBuf[14] = {0};
 
 /**
- * @brief Connect to a network and UDP socket.
- * 
- * This function will set-up the modem and connect an UDP socket.
- * 
- * @param apn The APN to use for the PDP context.
- * @param user The APN username.
- * @param pass The APN password.
- * @param ip The IP address of the server to connect to.
- * @param port The port to connect to.
- * 
- * @return True on success, false on error.
+ * @brief This function checks if we are connected to the lte network
+ *
+ * @return True when connected, False otherwise
  */
-bool connect(const char *apn, const char *user, const char *pass, const char *ip, uint16_t port)
-{
-  if(!modem.reset()) {
-    Serial.println("Could not reset the modem");
-    return false;
-  }
-
-  if(!modem.checkComm()) {
-    Serial.println("Modem communication error");
-    return false;
-  }
-
-  if(!modem.configCMEErrorReports()) {
-    Serial.println("Could not configure CME error reports");
-    return false;
-  }
-
-  if(!modem.configCEREGReports()) {
-    Serial.println("Could not configure CEREG reports");
-    return false;
-  }
-
-  WalterModemRsp rsp = {};
-  if(!modem.getOpState(&rsp)) {
-    Serial.println("Could not retrieve modem operational state");
-    return false;
-  }
-
-  if(modem.getRadioBands(&rsp)) {
-    Serial.println("Modem is configured for the following bands:");
-    
-    for(int i = 0; i < rsp.data.bandSelCfgSet.count; ++i) {
-      WalterModemBandSelection *bSel = rsp.data.bandSelCfgSet.config + i;
-      Serial.printf("  - Operator '%s' on %s: 0x%05X\n",
-        bSel->netOperator.name,
-        bSel->rat == WALTER_MODEM_RAT_NBIOT ? "NB-IoT" : "LTE-M",
-        bSel->bands);
-    }
-  } else {
-    Serial.println("Could not retrieve configured radio bands");
-    return false;
-  }
-
-  if(!modem.setOpState(WALTER_MODEM_OPSTATE_NO_RF)) {
-    Serial.println("Could not set operational state to NO RF");
-    return false;
-  }
-
-  /* Give the modem time to detect the SIM */
-  delay(2000);
-
-  if(modem.unlockSIM()) {
-    Serial.println("Successfully unlocked SIM card");
-  } else {
-    Serial.println("Could not unlock SIM card");
-    return false;
-  }
-
-  /* Create PDP context */
-  if(user != NULL) {
-    if(!modem.createPDPContext(
-        apn,
-        WALTER_MODEM_PDP_AUTH_PROTO_PAP,
-        user,
-        pass))
-    {
-      Serial.println("Could not create PDP context");
-      return false;
-    }
-  } else {
-    if(!modem.createPDPContext(apn)) {
-      Serial.println("Could not create PDP context");
-      return false;
-    }
-  }
-
-  /* Authenticate the PDP context */
-  if(!modem.authenticatePDPContext()) {
-    Serial.println("Could not authenticate the PDP context");
-    return false;
-  }
-
-  /* Set the operational state to full */
-  if(!modem.setOpState(WALTER_MODEM_OPSTATE_FULL)) {
-    Serial.println("Could not set operational state to FULL");
-    return false;
-  }
-
-  /* Set the network operator selection to automatic */
-  if(!modem.setNetworkSelectionMode(WALTER_MODEM_NETWORK_SEL_MODE_AUTOMATIC)) {
-    Serial.println("Could not set the network selection mode to automatic");
-    return false;
-  }
-
-  /* Wait for the network to become available */
+bool lteConnected() {
   WalterModemNetworkRegState regState = modem.getNetworkRegState();
-  while(!(regState == WALTER_MODEM_NETWORK_REG_REGISTERED_HOME ||
-          regState == WALTER_MODEM_NETWORK_REG_REGISTERED_ROAMING))
-  {
+  return (regState == WALTER_MODEM_NETWORK_REG_REGISTERED_HOME ||
+          regState == WALTER_MODEM_NETWORK_REG_REGISTERED_ROAMING);
+}
+
+/**
+ * @brief This function waits for the modem to be connected to the Lte network.
+ * @return true if the connected, else false on timeout.
+ */
+bool waitForNetwork() {
+  /* Wait for the network to become available */
+  int timeout = 0;
+  while (!lteConnected()) {
     delay(100);
-    regState = modem.getNetworkRegState();
+    timeout += 100;
+    if (timeout > 300000)
+      return false;
   }
   Serial.println("Connected to the network");
-
-  /* Activate the PDP context */
-  if(!modem.setPDPContextActive(true)) {
-    Serial.println("Could not activate the PDP context");
-    return false;
-  }
-
-  /* Attach the PDP context */
-  if(!modem.attachPDPContext(true)) {
-    Serial.println("Could not attach to the PDP context");
-    return false;
-  }
-
-  if(modem.getPDPAddress(&rsp)) {
-    Serial.println("PDP context address list: ");
-    Serial.printf("  - %s\n", rsp.data.pdpAddressList.pdpAddress);
-    if(rsp.data.pdpAddressList.pdpAddress2[0] != '\0') {
-      Serial.printf("  - %s\n", rsp.data.pdpAddressList.pdpAddress2);
-    }
-  } else {
-    Serial.println("Could not retrieve PDP context addresses");
-    return false;
-  }
-
-  /* Construct a socket */
-  if(!modem.createSocket(&rsp)) {
-    Serial.println("Could not create a new socket");
-    return false;
-  }
-
-  /* Configure the socket */
-  if(!modem.configSocket()) {
-    Serial.println("Could not configure the socket");
-    return false;
-  }
-
-  /* Connect to the UDP test server */
-  if(modem.connectSocket(ip, port, port)) {
-    Serial.printf("Connected to UDP server %s:%d\n", ip, port);
-  } else {
-    Serial.println("Could not connect UDP socket");
-    return false;
-  }
-
   return true;
 }
 
 /**
+ * @brief This function tries to connect the modem to the cellular network.
+ * @return true if the connection attempt is successful, else false.
+ */
+bool lteConnect() {
+  if (modem.setOpState(WALTER_MODEM_OPSTATE_NO_RF)) {
+    Serial.println("Successfully set operational state to NO RF");
+  } else {
+    Serial.println("Error: Could not set operational state to NO RF");
+    return false;
+  }
+
+  /* Create PDP context */
+  if (modem.definePDPContext()) {
+    Serial.println("Created PDP context");
+  } else {
+    Serial.println("Error: Could not create PDP context");
+    return false;
+  }
+
+  /* Set the operational state to full */
+  if (modem.setOpState(WALTER_MODEM_OPSTATE_FULL)) {
+    Serial.println("Successfully set operational state to FULL");
+  } else {
+    Serial.println("Error: Could not set operational state to FULL");
+    return false;
+  }
+
+  /* Set the network operator selection to automatic */
+  if (modem.setNetworkSelectionMode(WALTER_MODEM_NETWORK_SEL_MODE_AUTOMATIC)) {
+    Serial.println("Network selection mode to was set to automatic");
+  } else {
+    Serial.println(
+        "Error: Could not set the network selection mode to automatic");
+    return false;
+  }
+
+  return waitForNetwork();
+}
+
+/**
  * @brief Read out the relative humidity.
- * 
+ *
  * This function reads the relative humidity from the Si7006 sensor.
- * 
+ *
  * @return The resulting relative humidity.
  */
-float getRelHum()
-{
-  uint8_t data[2] = { 0 };
+float getRelHum() {
+  uint8_t data[2] = {0};
 
   Wire.beginTransmission(SI7006_ADDR);
   Wire.write(0xF5);
   Wire.endTransmission();
   delay(500);
-  
+
   Wire.requestFrom(SI7006_ADDR, 2);
-  if(Wire.available() == 2) {
+  if (Wire.available() == 2) {
     data[0] = Wire.read();
     data[1] = Wire.read();
   }
-  
-  float humidity  = ((data[0] * 256.0) + data[1]);
+
+  float humidity = ((data[0] * 256.0) + data[1]);
   humidity = ((125 * humidity) / 65536.0) - 6;
   return humidity;
 }
 
 /**
  * @brief Read out the temperature.
- * 
+ *
  * This function reads the temperature from the Si7006 sensor.
- * 
+ *
  * @return The resulting temperature.
  */
-static float getTemp()
-{
-  uint8_t data[2] = { 0 };
+static float getTemp() {
+  uint8_t data[2] = {0};
 
   Wire.beginTransmission(SI7006_ADDR);
   Wire.write(0xF3);
   Wire.endTransmission();
   delay(500);
-  
+
   Wire.requestFrom(SI7006_ADDR, 2);
-  if(Wire.available() == 2) {
+  if (Wire.available() == 2) {
     data[0] = Wire.read();
     data[1] = Wire.read();
   }
 
-  float temp  = ((data[0] * 256.0) + data[1]);
+  float temp = ((data[0] * 256.0) + data[1]);
   float ctemp = ((175.72 * temp) / 65536.0) - 46.85;
   return ctemp;
 }
 
 /**
  * @brief Set-up the system.
- * 
+ *
  * This function will setup the system and initialize the communication buses.
- * 
+ *
  * @return None.
  */
-void setup()
-{
+void setup() {
   Serial.begin(115200);
   delay(5000);
 
-  Serial.println("Walter SensorShield v0.0.1");
+  Serial.println("Walter SensorShield v1.0.0");
 
   /* Get the MAC address for board validation */
   esp_read_mac(dataBuf, ESP_MAC_WIFI_STA);
-  Serial.printf("Walter's MAC is: %02X:%02X:%02X:%02X:%02X:%02X\n",
-    dataBuf[0],
-    dataBuf[1],
-    dataBuf[2],
-    dataBuf[3],
-    dataBuf[4],
-    dataBuf[5]);
+  Serial.printf("Walter's MAC is: %02X:%02X:%02X:%02X:%02X:%02X\n", dataBuf[0],
+                dataBuf[1], dataBuf[2], dataBuf[3], dataBuf[4], dataBuf[5]);
 
   /* I2C bus initialisation */
   Wire.begin(I2C_SDA_PIN, I2C_SCL_PIN);
-  
+
   /* Lux sensor initialization */
   ltr329.begin();
-  ltr329.writetControl(true, LTR329ALS01_ALS_GAIN_x8); 
-  ltr329.writeMeasRate(LTR329ALS01_ALS_INT_100ms, LTR329ALS01_ALS_RATE_500ms); 
+  ltr329.writetControl(true, LTR329ALS01_ALS_GAIN_x8);
+  ltr329.writeMeasRate(LTR329ALS01_ALS_INT_100ms, LTR329ALS01_ALS_RATE_500ms);
 
   /* Baromether initialization */
-  if(!mpl3115a2.begin()) {
-    while(1);
+  if (!mpl3115a2.begin()) {
+    while (1)
+      ;
   }
   mpl3115a2.setSeaPressure(1013.26);
 
   /* Modem initialization */
-  if(modem.begin(&Serial2)) {
+  if (modem.begin(&Serial2)) {
     Serial.println("Modem initialization OK");
   } else {
-    Serial.println("Modem initialization ERROR");
+    Serial.println("Error: Modem initialization ERROR");
+    return;
+  }
+
+  /* Connect the modem to the lte network */
+  if (!lteConnect()) {
+    ESP_LOGE(TAG, "Could Not Connect to LTE");
+    return;
+  }
+
+  /* Construct a socket */
+  if (!modem.createSocket(&rsp)) {
+    Serial.println("Error Could not create a new socket");
+    return;
+  }
+
+  /* Connect to the UDP test server */
+  if (modem.socketDial(SERV_ADDR, SERV_PORT)) {
+    Serial.printf("Connected to UDP server %s:%d\n", ip, port);
+  } else {
+    Serial.println("Could not connect UDP socket");
     return;
   }
 }
 
-void loop()
-{
- float hum = getRelHum();
- float temp = getTemp();
- float lux = ltr329.readLux();
- float pressure = mpl3115a2.getPressure();
-  
+void loop() {
+  float hum = getRelHum();
+  float temp = getTemp();
+  float lux = ltr329.readLux();
+  float pressure = mpl3115a2.getPressure();
+
   uint16_t rawHum = hum * 100;
   uint16_t rawTemp = (temp + 50) * 100;
   uint16_t rawLux = lux * 10;
@@ -384,18 +307,14 @@ void loop()
   dataBuf[11] = rawLux & 0xFF;
   dataBuf[12] = rawPressure >> 8;
   dataBuf[13] = rawPressure & 0xFF;
-    
-  if(!connect("soracom.io", "sora", "sora", SERV_ADDR, SERV_PORT)) {
-    ESP.restart();
-  }
 
-  if(!modem.socketSend(dataBuf, dataSize)) {
+  if (!modem.socketSend(dataBuf, dataSize)) {
     Serial.println("Could not transmit data");
     ESP.restart();
   }
 
-  Serial.printf("Sent data %.01f %%, %.01f C, %.01f lux, %.01f hPa\n",
-    hum, temp, lux, pressure);
+  Serial.printf("Sent data %.01f %%, %.01f C, %.01f lux, %.01f hPa\n", hum,
+                temp, lux, pressure);
 
   delay(60000);
   ESP.restart();
